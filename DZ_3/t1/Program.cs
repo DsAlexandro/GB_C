18475045a11128f01efa3848048406d1c117b602
Alexandro <scherbakov857@gmail.com>
2022-11-03 16:33:21 +0500

    исправлена третья задача

--- DZ_3/t1/Program.cs
+++ DZ_3/t1/Program.cs
@@ -2,8 +2,8 @@
 Напишите программу, которая принимает на вход пятизначное число и проверяет,
 является ли оно палиндромом. Без использования строк*/
 
-
-static bool Palindrom(string s)
+//var 1
+/*static bool Palindrom(string s)
 {
     for (int i = 0; i < s.Length / 2; i++)
     {
@@ -19,3 +19,25 @@ static bool Palindrom(string s)
 Console.Write("Введите пятизначное число: ");
 string s = Console.ReadLine();
 Palindrom(s);
+*/
+//var 2 без строк
+
+void Palindrom(int number)
+{
+    if (number > 9999 && number <= 99999)
+    {
+        int num1  = number / 10000;
+        int num2  = number / 1000 % 10;
+        int num3  = number % 100 / 10;
+        int num4  = number % 10;
+        if(num1 == num4 && num2 == num3)
+        {
+            Console.WriteLine("Число " + number + " является палиндромом!");
+        }
+        else Console.WriteLine("Число " + number + " не является палиндромом!");
+    }
+    else Console.WriteLine("Введите пятизначное число !!! ");
+}
+Console.Write("Введите пятизначное число = ");
+int num = Convert.ToInt32(Console.ReadLine());
+Palindrom(num);
