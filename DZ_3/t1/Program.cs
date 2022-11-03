aeef56e417f37526f7fd741a98b95f37326348de
Alexandro <scherbakov857@gmail.com>
2022-11-03 15:30:01 +0500

    add DZ_3

---
+++ DZ_3/t1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 1
+Напишите программу, которая принимает на вход пятизначное число и проверяет,
+является ли оно палиндромом. Без использования строк*/
+
+
+static bool Palindrom(string s)
+{
+    for (int i = 0; i < s.Length / 2; i++)
+    {
+        if (s[i] != s[s.Length - i - 1])
+        {
+           Console.WriteLine("NO!");     
+           return false;
+        }
+    }
+    Console.WriteLine("YES!");
+    return true;
+}
+Console.Write("Введите пятизначное число: ");
+string s = Console.ReadLine();
+Palindrom(s);
