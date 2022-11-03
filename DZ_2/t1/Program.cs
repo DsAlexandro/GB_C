f2eae44d61eb7fa91f1ea6a482edba3f703f2f7e
Alexandro <scherbakov857@gmail.com>
2022-11-03 14:51:05 +0500

    add DZ_2

---
+++ DZ_2/t1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 1Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+// вторую цифру этого числа. Не использовать строки для расчета.
+void Num_2(int number)
+{
+    if(number/100 > 9 || number/10 < 9 )
+    {
+        Console.WriteLine("Просили же трёхзначное число!!!!");
+        
+    }
+    else
+    {
+        int num_2 = number / 10 % 10;
+        Console.WriteLine("Вторая цифра - " + num_2);
+        
+    }
+}
+Console.Write("Введите трёхзначное число - ");
+int number = int.Parse(Console.ReadLine());
+Num_2(number);
+
---
+++ DZ_2/t3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3 Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет. Не использовать строки для расчета.
+
+void num_3(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет!");
+    }
+    else
+    {
+        while(number > 1000)
+        {
+            number = number / 10;
+        }
+        int num_3 = number % 10;
+        Console.WriteLine("Третья цифра = " + num_3);
+}
+}
+Console.Write("Введите число - ");
+int number = int.Parse(Console.ReadLine());
+num_3(number);
